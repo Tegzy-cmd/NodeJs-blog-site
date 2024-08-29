ce98aa9d17b5eebd19e132a54de9a0d3c5aaa729
Tegzy-cmd <omorakabenjamin3@gmail.com>
2024-08-29 01:58:52 +0100

    reading dummy data from database

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,20 +1,26 @@
-const express = require ('express');
+const { errorMonitor } = require("connect-mongo");
+const express = require("express");
 const router = express.Router();
+const Post = require("../../models/Post");
+/**
+ * GET /
+ * HOME
+ */
 
-// Routes
-router.get('', (req,res)=>{
+router.get("", async (req, res) => {
+  const locals = {
+    title: "Node Js Blog",
+    description: "Simple Blog created with NodeJs, Express & MongoDb",
+  };
 
-    const locals = {
-
-        title:"Node Js Blog",
-        description: "Simple Blog created with NodeJs, Express & MongoDb"
-
-    }
-
-
-    res.render('index',{locals});
+  try {
+    const data = await Post.find();
+    res.render("index", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-
+// insertPostData()
 
 module.exports = router;
