ac5a6761e7ab7ab8ecb91ceb3112d9e00d9cf5b3
Tegzy-cmd <omorakabenjamin3@gmail.com>
2024-08-30 01:45:41 +0100

    added pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -8,18 +8,47 @@ const Post = require("../../models/Post");
  */
 
 router.get("", async (req, res) => {
-  const locals = {
-    title: "Node Js Blog",
-    description: "Simple Blog created with NodeJs, Express & MongoDb",
-  };
-
   try {
-    const data = await Post.find();
-    res.render("index", { locals, data });
+    const locals = {
+      title: "Node Js Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb",
+    };
+    let perPage = 5;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null
+    });
   } catch (error) {
     console.log(error);
   }
 });
+
+// router.get("", async (req, res) => {
+//     const locals = {
+//       title: "Node Js Blog",
+//       description: "Simple Blog created with NodeJs, Express & MongoDb",
+//     };
+
+//     try {
+//       const data = await Post.find();
+//       res.render("index", { locals, data });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
 
 // insertPostData()
 
